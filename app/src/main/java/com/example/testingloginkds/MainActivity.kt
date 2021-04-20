ee2f5f55f7c5bed0feeb2d427ec5b5f8e1801c43
Mr777Nick <bagas.insani@yummycorp.com>
2021-04-21 01:24:43 +0700

    Added custom Image View

--- app/src/main/java/com/example/testingloginkds/MainActivity.kt
+++ app/src/main/java/com/example/testingloginkds/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.testingloginkds
 
+import android.graphics.Matrix
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun setBackgroundTopSize() {
+        val imageView: ImageView = findViewById(R.id.imageView);
+        val matrix: Matrix = imageView.imageMatrix;
+        val imageWidth: Float = imageView.drawable.intrinsicWidth.toFloat();
+        val screenWidth: Int = resources.displayMetrics.widthPixels;
+        val scaleRatio: Float = screenWidth / imageWidth;
+        matrix.postScale(scaleRatio, scaleRatio);
+        imageView.imageMatrix = matrix;
     }
 }
